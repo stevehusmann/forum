bfe26e6a92b97183ac49b4e815bdb300b8887257
Steve Husmann <stevehoose@Steves-iMac.local>
2021-08-04 00:54:05 -0500

    this is confusing

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
-var button = document.getElementById('submit');
-
-button.addEventListener('click', function () {
+document.getElementById('submit').addEventListener('click', function () {
   var userName = document.getElementById('name').value;
   var userMessage = document.getElementById('message').value;
-  
+
+  var newPostDiv = document.getElementsByClassName('posts')[0];
+  var postMessage = document.createElement('p');
+  var messageContent = document.createTextNode(userMessage);
+  postMessage.appendChild(messageContent);
+
+  var postAuthor = document.createElement('strong');
+  var messageAuthor = document.createTextNode(userName);
+  postAuthor.appendChild(messageAuthor);
+
+  var postedBy = document.createElement('p');
+  var postedTag = document.createTextNode(`Posted By:`);
+  postedBy.appendChild(postedTag);
+
+  var postHR = document.createElement('hr');
+
+  newPostDiv.append(postMessage);
+  newPostDiv.append(postedBy);
+  newPostDiv.append(postAuthor);
+  newPostDiv.append(postHR);
 });
+
+
+
+  // //postMessage.appendChild(messageContent);
+  // document.getElementsByClassName('posts')[0].append(postMessage);
+  // //postAuthor.appendChild(messageAuthor);
+  // document.getElementsByClassName('posts')[0].append(postAuthor);
+
+  // // newPostDiv.append(postMessage);
+  // // newPostDiv.append(postAuthor);
+  // // newPostDiv.append(postHR);
+  
+  // // postsDiv.append(newPostDiv);
